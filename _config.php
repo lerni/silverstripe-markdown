2c51e08c94d91658c6065e7c31b001be0ea62ddd
Lukas <lerni@users.noreply.github.com>
2021-07-09 10:46:16 +0200

    php 7.x Object alias

--- _config.php
+++ _config.php
@@ -1,9 +1,11 @@
 <?php
 
+if (!class_exists('SS_Object')) class_alias('Object', 'SS_Object');
+
 define('MARKDOWN_MODULE_BASE', basename(dirname(__FILE__)));
 
 if(ClassInfo::exists('CloudinaryFileField')){
     MarkdownEditorField::add_extension('MarkdownCloudinaryUpload');
 }
 
-Object::useCustomClass('HtmlEditorField_Toolbar', 'MarkdownEditorField_Toolbar', true);
+SS_Object::useCustomClass('HtmlEditorField_Toolbar', 'MarkdownEditorField_Toolbar', true);
